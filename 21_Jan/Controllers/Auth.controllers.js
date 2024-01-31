4aeb334da3a2eb461e1d2c2648a99b92fff1509b
srushtimane27 <srushtimane31@gmail.com>
2024-01-31 15:20:59 +0530

    HashedPassword

--- 21_Jan/Controllers/Auth.controllers.js
+++ 21_Jan/Controllers/Auth.controllers.js
@@ -1,7 +1,9 @@
 import UserSchema from "../Model/User.schema.js";
+import bcrypt from 'bcrypt';
 
 export const register = async (req, res) => {
-    const { name, email, password, confirmPassword } = req.body;
+    try{
+        const { name, email, password, confirmPassword } = req.body;
     if(!name || !email || !password  || !confirmPassword){
         return res.send("All fields are required")
     }
@@ -14,17 +16,49 @@ export const register = async (req, res) => {
     if (isEmailExist.length){
         return res.send("Email is exist")
     }
+
+    const hashedPassword = await bcrypt.hashSync(password, 10)
+    // console.log(hashedPassword, password)
     
     const user = new UserSchema({
         name: name,
         email: email,
-        password: password,
-        confirmPassword: confirmPassword
+        password: hashedPassword
+        // confirmPassword: confirmPassword
     })
     await user.save()
-    res.send("User Succesfully Stored in DB")
+    return res.send("User Succesfully Stored in DB")
 
 
     // res.send(name)
     // res.send("Register")
+
+    } catch (error){
+        return res.status(500).json({ error })
+    } 
+}
+
+export const login = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        if(!email || !password){
+            return res.status(401).json({success: false, message: "Email and password both are required."})
+        }
+
+        const user = await UserSchema.findOne({ email: email })
+        if(!user) {
+            return res.status(404).json({ success: false, message: "User not exists."})
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordCorrect){
+            return res.status(404).json({success: false, message: "Password is incorrect"})
+        }
+
+        return res.status(200).json({ success: true, message: "Login Successfull"})
+    }
+    catch(error){
+        return res.status(500).json({ success: false, error: error})
+    }
 }
--- 21_Jan/routes/AuthRoutes.js
+++ 21_Jan/routes/AuthRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { register } from "../Controllers/Auth.controllers.js";
+import { register, login } from "../Controllers/Auth.controllers.js";
 
 const router = Router();
 
 router.post('/register', register);
+router.post('/login', login);
 
 export default router;
